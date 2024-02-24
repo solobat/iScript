6138fcda1e8cb3022585f9d3741ae5b8981c1906
solobat <solopea@icloud.com>
2024-02-24 22:22:19 +0800

    chore: git

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RunAt = void 0;
-var RunAt;
-(function (RunAt) {
-    RunAt[RunAt["START"] = 0] = "START";
-    RunAt[RunAt["END"] = 1] = "END";
-    RunAt[RunAt["IDLE"] = 2] = "IDLE";
-})(RunAt = exports.RunAt || (exports.RunAt = {}));
